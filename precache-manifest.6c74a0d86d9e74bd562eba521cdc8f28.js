68b3c85877117a8ea24010df944c28273b56a0ec
SHDGMSQ <sh.dg.msq@mail.ru>
2022-02-18 22:22:26 +0300

    Updates

---
+++ precache-manifest.6c74a0d86d9e74bd562eba521cdc8f28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda80cf6f429ee34801e6e76ddfa2bc7",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "841453d7faa7a4c125e8",
+    "url": "/Counter/static/css/main.123fb9b6.chunk.css"
+  },
+  {
+    "revision": "2de51c1014bd87cf9982",
+    "url": "/Counter/static/js/2.29632cbb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Counter/static/js/2.29632cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "841453d7faa7a4c125e8",
+    "url": "/Counter/static/js/main.e35823e3.chunk.js"
+  },
+  {
+    "revision": "60d75ed92e1196d63d74",
+    "url": "/Counter/static/js/runtime-main.c2fba775.js"
+  }
+]);
